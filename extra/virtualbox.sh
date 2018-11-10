eb8701678f57b04ffca6c7c66389f2ebde6a7206
emmett1 <emmett1.2miligrams@gmail.com>
2018-11-10 18:11:47 +0800

    updated

--- extra/virtualbox.sh
+++ extra/virtualbox.sh
@@ -8,7 +8,7 @@ version=$version-$_release
 
 echo -ne "$name ... "
 
-majversion=$(wget -qO - http://download.virtualbox.org/virtualbox/ | cut -d '"' -f2 | sort -V | grep ^[0-9] | sort -V | tail -n1 | sed 's/\///')
+majversion=$(wget -qO - http://download.virtualbox.org/virtualbox/ | cut -d '"' -f2 | sort -V | grep ^[0-9] | grep -vi beta | grep -vi alpha | tail -n1 | sed 's/\///')
 upversion=$(wget -qO - http://download.virtualbox.org/virtualbox/$majversion | grep Linux_amd64.run | cut -d '"' -f2 | sed 's/VirtualBox-//;s/-Linux_amd64.run//')
 
 result
