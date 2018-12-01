54db15d1f8564a90ae8323c0a88d29d92b4fdf1b
emmett1 <emmett1.2miligrams@gmail.com>
2018-12-01 23:43:12 +0800

    updated

---
+++ extra/streamlink.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+. pkgupdate
+
+echo -ne "$name ... "
+
+upversion=$(wget -qO - -t 3 -T 10 https://github.com/$NAME/$NAME/releases | grep ${NAME}-[0-9].*${EXT} | sed "s/.*\($NAME-[0-9].*$EXT\).*/\1/" | awk '{print $1}' | grep -v beta | grep -v alpha | sed "s/$NAME-//;s/$EXT//" | sort -V | tail -n1)
+
+result
+
+exit 0
